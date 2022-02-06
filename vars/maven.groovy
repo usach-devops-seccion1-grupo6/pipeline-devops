41ae02175be25bed2a91a0f03e904a18b166d6a6
Alejandro Carrasco <aicnavarrete@gmail.com>
2022-02-06 19:49:18 -0300

    gitDiff maven.groovy

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -115,8 +115,8 @@ def gitTagMaster(){
 }
 
 def gitDiff(){
-	sh "mkdir /tmp/${new_dir}"
-	sh "cd /tmp/${new_dir}"
+	sh "mkdir /tmp/new_dir"
+	sh "cd /tmp/new_dir"
 	sh "git clone --single-branch ${env.GIT_URL}"
 	def main_hash = sh "git rev-parse HEAD"
 	sh "git diff ${env.GIT_COMMIT} ${main_hash}"
