f190a8ba017a56327f9ae32ddd73b3b001bf66ca
Gonzalo Muñoz Boisier <gmboisier@gmail.com>
2022-02-06 18:35:03 -0300

    Actualiza gradle

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -73,7 +73,7 @@ def nexusCI(){
 }
 
 def downloadNexus(){
-	sh 'curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD "http://nexus:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/${env.NEXT_TAG}-develop/DevOpsUsach2020-${env.NEXT_TAG}-develop.jar" -O'
+	sh "curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD 'http://nexus:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/${env.NEXT_TAG}-develop/DevOpsUsach2020-${env.NEXT_TAG}-develop.jar' -O"
 }
 
 def runDownloadedJar(){
