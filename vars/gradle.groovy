1feaada9935ab6f8875934f249016ff587f11d82
Gonzalo Muñoz Boisier <gmboisier@gmail.com>
2022-02-06 17:09:02 -0300

    Actualiza gradle

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -7,12 +7,10 @@ def call(String chosenStages){
 	def pipelineType = (utils.isCIorCD().contains('ci')) ? 'IC' : 'Release'
 	def tags = sh(script: "git tag --sort version:refname | tail -1", returnStdout: true).trim()
 	echo "Git current tags: ${tags}"
-	tags = utils.upTagVersion("${tags}")}
-
+	tags = utils.upTagVersion("${tags}")
 	env.NEXT_TAG = "${tags}"
-
 	echo "Git new tags: ${env.NEXT_TAG}"
-
+	
 	def pipelineStages = (pipelineType == 'IC') ? ['buildAndTest','sonar','runJar','rest','nexusCI','gitCreateRelease'] : ['downloadNexus','runDownloadedJar','rest','nexusCD','gitMergeMaster','gitMergeDevelop','gitTagMaster']
 	def stages = utils.getValidatedStages(chosenStages, pipelineStages)
 
