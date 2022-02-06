3fc8820a9b58f9d2bd815ed4e72f1d6c1c5f72eb
Gonzalo Muñoz Boisier <gmboisier@gmail.com>
2022-02-06 19:44:00 -0300

    Actualiza gradle

--- bin/main/gradle.groovy
+++ bin/main/gradle.groovy
@@ -4,6 +4,8 @@ import pipeline.*
 def call(String chosenStages){
 
 	def utils  = new test.UtilMethods()
+	sh 'git switch realise-v1-1-0'
+	sh 'git pull all'
     def tags = sh(script: "git tag --sort version:refname | tail -1", returnStdout: true).trim()
 	env.CURR_TAG = "${tags}"
     echo "Git current tags: ${env.CURR_TAG}"
