0fbb2633c5b01debad25a75ac149b932faa2a766
Eduardo Cabrera <lalo@eof.cl>
2022-02-06 21:21:09 -0300

    se modifica maven

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,8 +1,10 @@
 import pipeline.*
 
 def call(String chosenStages){
+
 	def utils  = new test.UtilMethods()
-	def tags = sh(script: "git tag --sort version:refname | tail -1", returnStdout: true).trim()
+	sh "git pull --all"
+    def tags = sh(script: "git tag --sort version:refname | tail -1", returnStdout: true).trim()
 	env.CURR_TAG = "${tags}"
     echo "Git current tags: ${env.CURR_TAG}"
 	tags = utils.upTagVersion("${tags}")
@@ -10,23 +12,24 @@ def call(String chosenStages){
 	echo "Git new tags: ${env.NEXT_TAG}"
 	def pipelineType = (utils.isCIorCD().contains('ci')) ? 'IC' : 'Release'
 
-    def pipelineStages = (pipelineType == 'IC') ? ['compile', 'unitTest', 'jar', 'sonar','runJar','test','nexusUpload', 'nexusDownload', 'md5Jar', 'gitCreateRelease'] : ['gitDiff', 'nexusDownload','runJarDownload','test', 'gitMergeMaster','gitMergeDevelop','gitTagMaster']
-    def stages = utils.getValidatedStages(chosenStages, pipelineStages)
+	def pipelineStages = (pipelineType == 'IC') ? ['compile', 'unitTest', 'jar', 'sonar','runJar','test','nexusUpload', 'nexusDownload', 'md5Jar', 'gitCreateRelease'] : ['gitDiff', 'nexusDownload','runJarDownload','test', 'gitMergeMaster','gitMergeDevelop','gitTagMaster']
+	def stages = utils.getValidatedStages(chosenStages, pipelineStages)
 
-    env.PIPELINE_TYPE = "${pipelineType}"
+	env.PIPELINE_TYPE = "${pipelineType}"
+	env.TMP_FOLDER = "/tmp/${env.RAMA}"
+	utils.clone("${env.TMP_FOLDER}")
+	stages.each{
+		stage(it){
+			env.TAREA = "${it}"
 
-    stages.each{
-        stage(it){
-            env.TAREA = "${it}"
-
-            try {
-                "${it}"()
-            }
-            catch(Exception e) {
-                error "Stage ${it} tiene problemas: ${e}"
-            }
-        }
-    }
+			try {
+				"${it}"()
+			}
+			catch(Exception e) {
+				error "Stage ${it} tiene problemas: ${e}"
+			}
+		}
+	}
 }
 
 def compile(){
@@ -71,41 +74,45 @@ def nexusUpload(){
 			mavenAssetList: [
 				[classifier: '',
 				extension: 'jar',
-				filePath: 'build/DevOpsUsach2020-0.0.1.jar'
+				filePath: "build/libs/DevOpsUsach2020-${env.NEXT_TAG}.jar"
 			]
 		],
 			mavenCoordinate: [
 				artifactId: 'DevOpsUsach2020',
 				groupId: 'com.devopsusach2020',
 				packaging: 'jar',
-				version: "0.0.1-${env.GIT_BRANCH}"
+				version: "${env.NEXT_TAG}-${env.GIT_BRANCH}"
 			]
 		]
 	]
 }
 
 def nexusDownload(){
-    sh "curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD 'http://nexus:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1-develop/DevOpsUsach2020-0.0.1-${env.GIT_BRANCH}.jar' -O"
+	sh "curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD 'http://nexus:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/${env.NEXT_TAG}-${env.GIT_BRANCH}/DevOpsUsach2020-${env.NEXT_TAG}-${env.GIT_BRANCH}.jar' -O"
 }
 
-def runJarDownload(){
-	sh 'timeout 30 $(which nohup) java -jar DevOpsUsach2020-0.0.1-develop.jar 2>/dev/null>&1 &'
+def runDownloadedJar(){
+	sh "timeout 30 \$(which nohup) java -jar DevOpsUsach2020-${env.NEXT_TAG}-${env.GIT_BRANCH}.jar 2>/dev/null>&1 &"
     sleep 20
 }
 
+
 def gitCreateRelease(){
+	if("${env.BRANCH_NAME}" == "develop"){
+		sh "git fetch -p &&	git checkout develop && git pull && git checkout -b release-${env.NEXT_TAG} && git push origin release-${env.NEXT_TAG}"
+	}
 }
 
 def gitMergeMaster(){
+	sh "cd ${env.TMP_FOLDER} && git switch main && git pull && git merge -m 'merge to master' --no-ff origin/release-${env.NEXT_TAG} && git push"
 }
 
 def gitMergeDevelop(){
+	sh "cd ${env.TMP_FOLDER} && git fetch origin develop && git checkout develop && git merge -m 'merge to develop' --no-ff origin/release-${env.NEXT_TAG} && git push"
 }
 
 def gitTagMaster(){
-}
-
-def gitDiff(){
+	sh "cd ${env.TMP_FOLDER} && git fetch origin main && git checkout main && git tag -m 'create tag' -a ${env.NEXT_TAG} && git push --tags && git branch -d release-${env.NEXT_TAG} && git push -d origin release-${env.NEXT_TAG}"
 }
 
 return this;
